20fe9d358560eba6980f7cec75b54f4147a2c289
Jufrench <ju.french@gmail.com>
2024-12-31 17:53:20 -0500

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 
 import './index.css'
 import App from './App.tsx'
 import { MantineProvider } from '@mantine/core';
-// import Search from './components/Search.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
