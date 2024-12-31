62900e280d6f3ebe0da8a0a428958cf8938e5c1d
Jufrench <ju.french@gmail.com>
2024-12-30 20:19:45 -0500

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 
-// import MobileHeader from './components/MobileHeader';
 import QuickPlay from './components/QuickPlay';
 import './App.css'
-// import BottomNavBar from './components/BottomNavBar';
 
 function App() {
 
@@ -12,15 +10,9 @@ function App() {
 
   return (
     <MantineProvider>
-      {/* <header style={{ position: "fixed", top: 0, right: 0, left: 0 }}>
-        <MobileHeader />
-      </header> */}
       <main id="main">
         <QuickPlay />
       </main>
-      {/* <footer style={{ position: "fixed", right: 0, bottom: 0, left: 0 }}>
-        <BottomNavBar />
-      </footer> */}
     </MantineProvider>
   )
 }
