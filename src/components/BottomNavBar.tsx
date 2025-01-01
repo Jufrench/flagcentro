829631807bedcf781775dff72f4a6200f53d2994
Jufrench <ju.french@gmail.com>
2025-01-01 15:15:12 -0500

    Cleanup

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -6,11 +6,8 @@ export default function BottomNavBar() {
   return (
     <Group p="md" justify="space-between">
       <NavLink to="/" end><ActionIcon size="lg" variant="light"><IconHomeFilled /></ActionIcon></NavLink>
-      {/* <NavLink to="/search" end>Search</NavLink> */}
       <NavLink to="/search" end><ActionIcon size="lg" variant="light"><IconSearch /></ActionIcon></NavLink>
-      {/* <NavLink to="/multi" end>Multi</NavLink> */}
       <NavLink to="/multi" end><ActionIcon size="lg" variant="light"><IconFriends /></ActionIcon></NavLink>
-      {/* <NavLink to="/profile" end>Profile</NavLink> */}
       <NavLink to="/profile" end><ActionIcon size="lg" variant="light"><IconUser /></ActionIcon></NavLink>
     </Group>
   )
