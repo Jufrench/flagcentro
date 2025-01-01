2098e09d841ddf056584da40529a405f3946e677
Jufrench <ju.french@gmail.com>
2025-01-01 15:14:44 -0500

    Buttons for bottom nav bar

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,13 +1,17 @@
-import { Group } from "@mantine/core";
+import { ActionIcon, Group } from "@mantine/core";
 import { NavLink } from "react-router";
+import { IconFriends, IconHomeFilled, IconSearch, IconUser } from '@tabler/icons-react';
 
 export default function BottomNavBar() {
   return (
     <Group p="md" justify="space-between">
-      <NavLink to="/" end>Home</NavLink>
-      <NavLink to="/search" end>Search</NavLink>
-      <NavLink to="/multi" end>Multi</NavLink>
-      <NavLink to="/profile" end>Profile</NavLink>
+      <NavLink to="/" end><ActionIcon size="lg" variant="light"><IconHomeFilled /></ActionIcon></NavLink>
+      {/* <NavLink to="/search" end>Search</NavLink> */}
+      <NavLink to="/search" end><ActionIcon size="lg" variant="light"><IconSearch /></ActionIcon></NavLink>
+      {/* <NavLink to="/multi" end>Multi</NavLink> */}
+      <NavLink to="/multi" end><ActionIcon size="lg" variant="light"><IconFriends /></ActionIcon></NavLink>
+      {/* <NavLink to="/profile" end>Profile</NavLink> */}
+      <NavLink to="/profile" end><ActionIcon size="lg" variant="light"><IconUser /></ActionIcon></NavLink>
     </Group>
   )
 }
