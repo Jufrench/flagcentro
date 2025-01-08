e87b9ef728caedb3107f6f7f1d3731599c3d2bf4
Jufrench <ju.french@gmail.com>
2025-01-08 17:56:33 -0500

    Test enable scroll on modal

--- src/components/ModalWrapper.tsx
+++ src/components/ModalWrapper.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalProps } from "@mantine/core";
+import { Modal, ModalProps, ScrollArea } from "@mantine/core";
 
 interface ModalWrapperProps extends ModalProps {
 
@@ -6,7 +6,11 @@ interface ModalWrapperProps extends ModalProps {
 
 export default function ModalWrapper(props: ModalWrapperProps) {
   return (
-    <Modal onClose={props.onClose} opened={props.opened} title={props.title}>
+    <Modal
+      onClose={props.onClose}
+      opened={props.opened}
+      title={props.title}
+      scrollAreaComponent={ScrollArea.Autosize}>
       {props.children}
     </Modal>
   )
