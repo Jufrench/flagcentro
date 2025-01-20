4d57c1bf138bab4f2cabc2d4551a3bfd2364f58f
Jufrench <ju.french@gmail.com>
2025-01-20 17:14:42 -0500

    Trying to allow comments in code w/o production failing

--- src/components/FlagDisplay.tsx
+++ src/components/FlagDisplay.tsx
@@ -22,7 +22,6 @@ export type CountryItem = {
 }
 
 export default function FlagDisplay(props: FlagDisplayProps) {
-  console.log('%cprops:', 'background:limegreen', props)
   return (
     <Card p={0} shadow="md" style={{ border: "1px solid #ddd" }}>
       <Image src={props.activeCountry.flags.png} alt={props.activeCountry.flags.alt} />
