6a64c65e2bd8263d3a145d174459e0c138173413
Jufrench <ju.french@gmail.com>
2025-02-16 23:59:27 -0600

    Words text inputs wrap line when necessary

--- src/components/dailyplay/NameInputs.tsx
+++ src/components/dailyplay/NameInputs.tsx
@@ -1,13 +1,51 @@
-import { Group, TextInput } from "@mantine/core";
+import { Box, Group, TextInput } from "@mantine/core";
 
 interface NameInputsProps {
   countryName: string;
 }
 
 export default function NameInputs(props: NameInputsProps) {
+  const words = props.countryName.split(" ");
+  
   return (
-    <Group gap={1} justify="center">
-      {Array.from(props.countryName).map((char: string, index: number) => {
+    <Group
+      gap={6}
+      justify="center"
+      style={{
+        flexWrap: "wrap"
+      }}
+    >
+      {words.map((word: string) => {
+        return (
+          <Box
+            style={{
+              whiteSpace: "nowrap",
+              display: "flex"
+            }}
+          >
+            <>
+              {[...word].map((letter: string, index: number) => {
+                const key = letter + index;
+                return (
+                  <TextInput
+                    key={key}
+                    size="xl"
+                    style={{ width: "36px", margin: "2px 0" }}
+                    styles={{
+                      input: {
+                        padding: 0,
+                        textAlign: "center",
+                        fontSize: "1.25em"
+                      }
+                    }}
+                  />
+                )
+              })}
+            </>
+          </Box>
+        )
+      })}
+      {/* {Array.from(props.countryName).map((char: string, index: number) => {
         const key = "" + char + index;
         console.log('%cchar:', 'background:tomato', char)
 
@@ -30,9 +68,37 @@ export default function NameInputs(props: NameInputsProps) {
             }
           </>
         )
-      })}
+      })} */}
     </Group>
   )
+  // return (
+  //   <Group gap={1} justify="center">
+  //     {Array.from(props.countryName).map((char: string, index: number) => {
+  //       const key = "" + char + index;
+  //       console.log('%cchar:', 'background:tomato', char)
+
+  //       return (
+  //         <>
+  //           {char === " "
+  //             ? <span style={{ width: "26px" }}>{"\xa0"}</span>
+  //             : <TextInput
+  //                 key={key}
+  //                 size="xl"
+  //                 style={{ width: "26px", margin: "2px 0" }}
+  //                 styles={{
+  //                   input: {
+  //                     padding: 0,
+  //                     textAlign: "center",
+  //                     fontSize: "1.25em"
+  //                   }
+  //                 }}
+  //               />
+  //           }
+  //         </>
+  //       )
+  //     })}
+  //   </Group>
+  // )
 }
 
   // return (
