f53b28ec9ecb6afee79a58f5101936c0c61523b2
Jufrench <ju.french@gmail.com>
2025-07-14 23:42:25 -0500

    Remove mobile header burger, building out profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,30 @@
-import { Title } from "@mantine/core";
+import { Avatar, Button, Fieldset, Stack, TextInput, Title } from "@mantine/core";
 
 export default function Profile() {
+  const hasAccount = false;
+  
   return (
-    <Title>Profile Page</Title>
+    <>
+      {hasAccount
+        ?
+        <Stack align="center">
+          <Avatar size="lg" />
+          <Title order={4}>User Name</Title>
+        </Stack>
+        :
+        <Stack>
+          <Fieldset legend="User Info">
+            <Stack gap="xs">
+              <TextInput label="First name" placeholder="First name" />
+              <TextInput label="Last name" placeholder="Last name" />
+              <TextInput label="Email" placeholder="Email" />
+              <TextInput label="Password" placeholder="Password" />
+              <TextInput label="Confirm Password`" placeholder="Confirm Password" />
+            </Stack>
+          </Fieldset>
+          <Button>Creat Account</Button>
+        </Stack>
+      }
+    </>
   )
 }
--- src/components/headers/MobileHeader.tsx
+++ src/components/headers/MobileHeader.tsx
@@ -1,12 +1,12 @@
-import { Burger, Center, Text, useMantineTheme } from "@mantine/core";
-import { useDisclosure } from '@mantine/hooks';
+import { Center, Text } from "@mantine/core";
+// import { useDisclosure } from '@mantine/hooks';
 import { useLocation } from 'react-router'
 
-import SettingsDrawer from "../SettingsDrawer";
+// import SettingsDrawer from "../SettingsDrawer";
 
 export default function MobileHeader() {
-  const [opened, { open, close }] = useDisclosure(false);
-  const theme = useMantineTheme();
+  // const [opened, { open, close }] = useDisclosure(false);
+  // const theme = useMantineTheme();
 
   let location = useLocation();
   let pathname = location.pathname.slice(1);
@@ -27,9 +27,9 @@ export default function MobileHeader() {
 
   return (
     <Center p="sm">
-      <Burger onClick={open} style={{ position: "absolute", left: theme.spacing.md }}></Burger>
+      {/* <Burger onClick={open} style={{ position: "absolute", left: theme.spacing.md }}></Burger> */}
       <Text size="md">{displayPath}</Text>
-      <SettingsDrawer opened={opened} close={close} />
+      {/* <SettingsDrawer opened={opened} close={close} /> */}
     </Center>
   )
 }
